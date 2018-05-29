327d11d106f007c125455e782b2a9af9ee89015f
kartung <kartung@gmail.com>
2018-05-29 20:17:34 +0800

    Added PATCH functionality

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const _ = require('lodash');           // added for patch
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-const port = process.env.PORT || 3000;  //added
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -63,10 +64,38 @@ app.delete('/todos/:id', (req, res) => {
       return res.status(404).send();
     }
 
-    res.send(todo);
+    res.send({todo});     // amemded to include {}
   }).catch((e) => {
     res.status(400).send();
   });
+});
+
+// added for Patch
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);    //where updatable stored
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {    // is a boolean & is true
+    body.completedAt = new Date().getTime();      // set date of completion - JS time format
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+   // body is key_value pair set by operator $set.
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
 });
 
 
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,21 +1,18 @@
 const expect = require('expect');
 const request = require('supertest');
-const {ObjectID} = require('mongodb');    // added
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
-  _id: new ObjectID(),       // added
+  _id: new ObjectID(),
   text: 'First test todo'
 }, {
-  _id: new ObjectID(),       // added
+  _id: new ObjectID(),
   text: 'Second test todo'
 }];
 
-// beforeEach((done) => {
-//   Todo.remove({}).then(() => done());
-// });
 
 beforeEach((done) => {
   Todo.remove({}).then(() => {
@@ -100,6 +97,47 @@ describe('GET /todos/:id', () => {
   it('should return 404 for non-object ids', (done) => {
     request(app)
       .get('/todos/123abc')
+      .expect(404)
+      .end(done);
+  });
+});
+
+
+// added to test delete
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', (done) => {
+    var hexId = todos[1]._id.toHexString();
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.findById(hexId).then((todo) => {
+          expect(todo).toNotExist();
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    var hexId = new ObjectID().toHexString();
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 if object id is invalid', (done) => {
+    request(app)
+      .delete('/todos/123abc')
       .expect(404)
       .end(done);
   });
