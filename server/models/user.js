b0278fa506bf14d9610837d59d7a02dcacb6faa3
kartung <kartung@gmail.com>
2018-06-01 12:55:59 +0800

    Added login route

--- server/models/user.js
+++ server/models/user.js
@@ -72,6 +72,29 @@ UserSchema.statics.findByToken = function (token) {     //model method, not inst
   });
 };
 
+
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      // Use bcrypt.compare to compare password and user.password
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
+  });
+};
+
+
 // For salting password
 UserSchema.pre('save', function (next) {
   var user = this;
